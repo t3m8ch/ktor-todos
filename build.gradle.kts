86d20aa5727b49da1f7b59f01f815577d872b7a3
Kudyakov Artem <t3m8ch@yandex.ru>
2022-07-06 11:00:59 +0400

    build: Load config from .env flie

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.3.1")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/main/kotlin/io/github/t3m8ch/Application.kt
+++ src/main/kotlin/io/github/t3m8ch/Application.kt
@@ -1,11 +1,18 @@
 package io.github.t3m8ch
 
+import io.github.cdimascio.dotenv.Dotenv
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.github.t3m8ch.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+    val dotenv = Dotenv.load()
+
+    embeddedServer(
+        Netty,
+        port = dotenv["WEBAPP_PORT"].toInt() ?: 8000,
+        host = dotenv["WEBAPP_HOST"] ?: "localhost",
+    ) {
         configureRouting()
         configureSerialization()
     }.start(wait = true)
